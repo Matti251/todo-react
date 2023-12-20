775a962e9e576021edf8059c1ca1df68337b650a
Matti Ullah Thaheem <139385194+Matti251@users.noreply.github.com>
2023-12-20 18:11:39 +0500

    add aalert

--- src/App.js
+++ src/App.js
@@ -4,25 +4,35 @@ import Todolist from "./Todolist";
 const App = () => {
   //Hook && State
   const [inputList, setInputList] = useState(
-    "Watch Wrong Turn Movie"
+    ""
   );
   const [items, setItems] = useState([]);
   //onChange input Define
   const prevItem = (event) => {
     setInputList(event.target.value);
   };
+
   //store data btn array
-  const listItem = () => {
-    setItems([...items, inputList]);
-    setInputList("");
+  const listItem = (event) => {
+    try {
+      if (inputList === "") {
+        alert("Please enter a task");
+        return;
+      }
+      // Add item to the list
+      setItems([...items, inputList]);
+      setInputList("");
+    } catch (error) {
+      console.error(error);
+    }
   };
-  const deletItems = (val) => { 
-    setItems((oldItems) => { 
+  const deletItems = (val) => {
+    setItems((oldItems) => {
       return oldItems.filter((item) => {
         return item !== val;
       });
     });
-  }; 
+  };
   return (
     <>
       <div className="main_div">
@@ -36,14 +46,16 @@ const App = () => {
             onChange={prevItem}
             value={inputList}
           />
-          <button onClick={listItem}>+</button> 
-          <ol> 
+          <button onClick={listItem}>+</button>
+          <ol>
             {items.map((itemVal, index) => {
               return (
                 <Todolist
                   text={itemVal}
-                  key={index} 
-                  onSelect={()=>deletItems(itemVal)}
+                  key={index}
+                  onSelect={() =>
+                    deletItems(itemVal)
+                  }
                 />
               );
             })}
